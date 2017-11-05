f59ed8207064088a0e9fe2742a1737fc4f34f3eb
Squiz <squizlstv@gmail.com>
2017-11-05 21:25:41 +1030

    Create velocity.lua

---
+++ velocity.lua
@@ -0,0 +1,21 @@
+-- Made by Squiz
+-- "pk is not bhop" -iced coffee
+
+hook.Add( "HUDPaint", "HUDPaint_DrawABox", function()
+    local Vel = math.Round(LocalPlayer():GetVelocity():Length()/2)
+    surface.SetDrawColor( 0, 0, 0, 150 )
+    surface.DrawRect( ScrW()/120, ScrH()/90, math.Clamp((Vel/2)+25,0,1700), 32 )
+end)
+
+hook.Add("HUDPaint","HUDDraw_VelText",function()
+    local Vel = math.Round(LocalPlayer():GetVelocity():Length())
+    draw.Text({
+    text=Vel,
+    font="Trebuchet24",
+    pos={ScrW()/90,ScrH()/70},
+    xalign=0,
+    yalign=0,
+    color=Color(math.Clamp((math.sin(CurTime()/8)+1)*127.5,0,255), math.Clamp((math.sin(CurTime()/4)+1)*127.5,0,255) ,math.Clamp((math.sin(CurTime()/2)+1)*127.5,0,255),255)})
+end)
+
+
