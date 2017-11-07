58b5532c03d3cf807b6761308aad2c250fc0d728
Squiz <squizlstv@gmail.com>
2017-11-07 14:40:37 +1030

    Oof 2

--- addons/minecraft-deathsound/lua/autorun/oof.lua
+++ addons/minecraft-deathsound/lua/autorun/oof.lua
@@ -1,4 +1,4 @@
-CreateClientConVar( "death_sound", '"oof.mp3"', true, false, "Sound to Play when a player dies." )
+CreateClientConVar( "death_sound", 'oof.mp3', true, false, "Sound that should play when a player dies." )
 local death_sound = GetConVar( "death_sound" )
 
 function OverrideDeathSound()
@@ -9,7 +9,7 @@ function playerDies(player)
     player:EmitSound( death_sound:GetString() )
 end
 
-hook.Add("PlayerDeath", "playerDies", playerDies)
 hook.Add("PlayerDeathSound", "OverrideDeathSound", OverrideDeathSound)
+hook.Add("PlayerDeath", "playerDies", playerDies)
 
 
