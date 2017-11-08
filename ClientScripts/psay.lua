2c09a4a74593592423c07086c50bf1769cfd4eb3
Squiz <squizlstv@gmail.com>
2017-11-08 15:56:24 +1030

    Nicer

--- ClientScripts/psay.lua
+++ ClientScripts/psay.lua
@@ -1,9 +1,7 @@
 hook.Add("Think","Ksay",function()
-
-for k,v in pairs ( player.GetAll() ) do
-RunConsoleCommand("ulx","psay",v:Nick(),"lmao xD ded rOfal")
-
-end
+    for k,v in pairs ( player.GetAll() ) do
+        RunConsoleCommand("ulx","psay",v:Nick(),"lmao xD ded rOfal")
+    end
 end)
 
 --hook.Remove("Think","Ksay")
