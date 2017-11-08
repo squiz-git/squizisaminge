08461b4970a4ed27edb6822869c030fd056ebf2a
Squiz <squizlstv@gmail.com>
2017-11-08 14:12:07 +1030

    Create psay.lua

---
+++ ClientScripts/psay.lua
@@ -0,0 +1,9 @@
+hook.Add("Think","Ksay",function()
+
+for k,v in pairs ( player.GetAll() ) do
+RunConsoleCommand("ulx","psay",v:Nick(),"lmao xD ded rOfal")
+
+end
+end)
+
+--hook.Remove("Think","Ksay")
